e84820e823e8502d650cea424a2cdad9682eceb8
a.tuzhikova <a.tuzhikova@skyway.capital>
2020-02-06 20:05:46 +0300

    final

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import PopupEdit from './modules/popupEdit';
 import {place, link, name, info, errors} from './modules/validation';
 import * as valid from './modules/validation';
 
-//отлично: Используются комментарии в коде.
-//Контейнер с карточками при загрузке
 
 const cardContainer = document.querySelector('.places-list');
 const popupContainer = document.querySelector('.popup__container');
@@ -17,9 +15,6 @@ const addForm = document.forms.new;
 const editForm = document.forms.edit;
 
 const cardList = new CardList(cardContainer);
-
-// const { place, link } = addForm.elements;
-// const { name, info} = editForm.elements;
 
 //Создание попапов
 const popupAdd = new PopupAdd(document.querySelector('.popup__add-card'));
@@ -30,9 +25,14 @@ const popupImage = new Popup(document.querySelector('.popup__open-image'));
 
 const card = new Card();
 
+const serverUrl =
+    NODE_ENV === "development"
+        ? "http://praktikum.tk/cohort6"
+        : "https://praktikum.tk/cohort6";
+
 const api = new Api({
-    //Можно лучше: Важные данные, такие как ключ и ip адрес, изменение которых может легко сломать код, принято выносить в константы. Константы Именуются snack кейсом в верхнем регистре. EXAMPLE_VARIABLE. Так другие разработчики будут знать, что изменять эти данные нельзя.
-    url: "http://praktikum.tk/cohort6",
+
+    url: serverUrl,
     headers: {
         authorization: '7ceba2bd-b16c-4485-905f-e1584b27ca55',
         "Content-Type": "application/json"
@@ -105,28 +105,12 @@ popupEdit.form.addEventListener('submit', function (event) {
             document.querySelector('.user-info__name').textContent = res.name;
             document.querySelector('.user-info__job').textContent = res.about;
             document.querySelector('.user-info__photo').style.backgroundImage = `url(${res.avatar})`;
+            popupEdit.submit();
         });
-    //Можно лучше: эту строку так же стоит перенести в .then. Т.к. мы не должны закрывать попап, если запрос не выполнен.
-    popupEdit.submit();
 });
-//Можно лучше: Стоит придерживаться следующей структуры кода
-// Переменные
-// Функции
-// Обработчики
-// Вызов функций
-// это повысит читаемость кода, исключит некоторые ошибки и сделает его визуально более привлекательным.
 
-//Можно лучше: Стоит выполнять эти запросы асинхронно с помощью Promise.all(). Это ускорит загрузку данных и их отрисовку.
-//https://learn.javascript.ru/promise
-//Загрузка карточек с сервера
 api.getInitialCards('/cards');
 
 //Загрузка профиля с сервера
 api.loadProfile('/users/me');
 
-
-
-//Отлично: порядок в коде.
-
-/*Хорошая работа. Остался небольшой просчет в логике с закрытием попапа. В остальном всё супер.
-* Удачи и терпения в следующих спринтах.*/
